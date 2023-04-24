21ffcea22515e37a70aab30a20dbfcc5424b9ca5
uphong157 <uphong157@gmail.com>
2023-04-23 20:58:51 -0700

    Complete up to exercise 2.3

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,27 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p><b>total of {sum} exercises</b></p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => 
+  <>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+  </>
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total sum={course.parts.reduce(
+        (totalExercises, part) => totalExercises + part.exercises, 0
+      )} />
+    </div>
+  )
+}
+
+export default Course
