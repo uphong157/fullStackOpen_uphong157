513a07cb2041cc06df18a07b4bf17a904ada69e1
uphong157 <uphong157@gmail.com>
2023-04-29 22:06:03 -0700

    Complete exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,19 +6,33 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={e => setNewName(e.target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => 
+        <p key={person.name}>
+          {person.name}
+        </p>
+      )}
     </div>
   )
 }
